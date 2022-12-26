fea9ddd3a818d310ff007375cae9e07c27be6caf
kaic-sa <kaictacker@gmai.com>
2022-12-26 13:58:08 -0300

    starting modifications

--- my-app/src/Components/App/App.js
+++ my-app/src/Components/App/App.js
@@ -1,18 +1,19 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-export class App extends React.Component{
-render(){
-  <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <div className= "App-searchbar"></div>
-    <div className="App-playlist">
-    <div className ="App-searchresults"></div>
-     
-    </div>
-  </div>
-</div>
-}
+export class App extends React.Component {
+  render() {
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <div className="App-searchbar"></div>
+        <div className="App-playlist">
+          <div className="App-searchresults"></div>
+        </div>
+      </div>
+    </div>;
+  }
 }
