ec284eaa082fa3117cc5daa4d59056ccc9c5b421
kaic-sa <kaictacker@gmai.com>
2022-12-27 12:21:05 -0300

    Finalizando as mudanças de diretório e iniciando o projeto

--- my-app/src/Components/App/App.js
+++ my-app/src/Components/App/App.js
@@ -1,23 +1,24 @@
 import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App-searchbar from
-import App-playlist  from
-import App-searchresults from
-
+import { SearchBar } from "../SearchBar/SearchBar";
+//import App-playlist  from
+//import App-searchresults from
 
 export class App extends React.Component {
   render() {
-    <div>
-      <h1>
-        Ja<span className="highhlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <div className="App-searchbar"></div>
-        <div className="App-playlist">
-        <div className="App-searchresults"></div>
+    return (
+      <div>
+        <h1>
+          Ja<span className="highhlight">mmm</span>ing vamos nós
+        </h1>
+        <div className="App">
+          <div className="App-searchbar"></div>
+          <div className="App-playlist">
+            <div className="App-searchresults"></div>
+          </div>
         </div>
       </div>
-    </div>;
+    );
   }
 }
