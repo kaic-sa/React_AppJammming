d3c05a91acaebee469d1426b7d24d3883adae2a5
Kaic Santos <kaictacker@gmail.com>
2022-12-27 22:44:59 -0300

    Tracks component added

---
+++ my-app/src/Components/Track/Track.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export class Track extends React.Component{
+   
+    
+    render(){ 
+        
+   { //<div class="Track">
+    //<div class="Track-information">
+      //<h3>track name will go here </h3>
+      //<p><!-- track artist will go here--> | <!-- track album will go here --></p>
+   // </div>
+  //  <button class="Track-action"><!-- + or - will go here --></button>
+  //</div>
+ // </div>}
+    
+}
+}
+}
